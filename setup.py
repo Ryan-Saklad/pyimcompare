da9c55620817ac033f0b957af4c30074bcd80e65
Ryan-Saklad <43081507+Ryan-Saklad@users.noreply.github.com>
2019-01-24 15:52:10 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pyimcompare',
+    author='Ryan-Saklad',
+    install_requires=['opencv-python', 'pyautogui'],
+    packages=['pyimcompare'],
+    url='https://github.com/Ryan-Saklad/pyimcompare',
+    version='0.1'
+)
